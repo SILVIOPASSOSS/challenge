f01693eeafe0af06373b0fcb26e6337efb0428db
Silvio Passos <silvyopassosdossantos@hotmail.com>
2022-07-15 13:26:44 -0300

    feat: configure SPA routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,12 +11,28 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/register',
+    name: 'registerStudent',
+    component: () => import('../components/students/Register.vue')
+  },
+  {
+    path: '/edit/:id',
+    name: 'editStudent',
+    component: () => import('../components/students/Edit.vue')
+  },
+  {
+    path: '/detail/:id',
+    name: 'detailStudent',
+    component: () => import('../components/students/Detail.vue')
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
