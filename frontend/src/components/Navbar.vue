0f2e3655fd85ccdffa3c7fa761e44dc2c04ffb3f
Silvio Passos <silvyopassosdossantos@hotmail.com>
2022-07-15 13:45:29 -0300

    feat: create navbar
    First versio, no customization

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-app id="inspire">
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Grupo A
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Módulo Acadêmico
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- Evento de Click  -->
+    <!-- <v-app-bar app color="#EF5350" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-tittle>Nome da página aqui</v-toolbar-tittle>
+    </v-app-bar> -->
+
+    <v-app-bar
+      app
+      color="indigo darken-2"
+      dark
+      prominent
+    >
+      <v-app-bar-nav-icon  @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Title</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Alunos', icon: 'mdi-account', to:'/' },
+          { title: 'Sobre', icon: 'mdi-help-box', to:'/about' },
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
+
+<style>
+.v-toolbar__title {
+  font-size: 1rem !important;
+}
+.v-badge__badge {
+  font-size: 10px !important;
+  height: 18px !important;
+  min-width: 18px !important;
+}
+</style>
