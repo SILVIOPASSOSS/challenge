e13ebc14f839224e3c136b6916342eb08018f665
Silvio Passos <silvyopassosdossantos@hotmail.com>
2022-07-16 00:45:16 -0300

    feat: add new layout footer
    Various visual tweaks

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer dark padless>
+    <v-card class="flex grey lighten-5 dark" flattile color="black">
+      <v-card-text class="py-2 black--text text-center">
+        {{ new Date().getFullYear() }} — <strong>Grupo A+</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
