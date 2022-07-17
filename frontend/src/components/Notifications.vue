ea3e5b2575fe37a63fec90e4d5e50fe33cac3eae
Silvio Passos <silvyopassosdossantos@hotmail.com>
2022-07-17 00:46:09 -0300

    feat: added notifications of feedback

---
+++ frontend/src/components/Notifications.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app id="inspire">
+    <v-card
+      height="300"
+      flat
+    >
+    <p>OI</p>
+      <v-snackbar
+        :timeout="-1"
+        :value="true"
+        absolute
+        bottom
+        color="success"
+        outlined
+        right
+      >
+        snackbar with <strong>outlined</strong> property.
+      </v-snackbar>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "notification"
+
+}
+</script>
+
