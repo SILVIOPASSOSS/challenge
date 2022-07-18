70a4b5307243ada06e46f5047b1a2b034fb1d8a0
Silvio Passos <silvyopassosdossantos@hotmail.com>
2022-07-18 13:39:16 -0300

    feat: added dynamic tittle

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,26 +8,41 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      title: 'Consulta de Aluno'
+    }
   },
   {
     path: '/register',
     name: 'registerStudent',
-    component: () => import('../components/students/Register.vue')
+    component: () => import('../components/students/Register.vue'),
+    meta: {
+      title: 'Cadastro de Aluno'
+    }
   },
   {
     path: '/edit/:id',
     name: 'editStudent',
-    component: () => import('../components/students/Edit.vue')
+    component: () => import('../components/students/Edit.vue'),
+    meta: {
+      title: 'Editar Aluno'
+    }
   },
   {
     path: '/detail/:id',
     name: 'detailStudent',
-    component: () => import('../components/students/Detail.vue')
+    component: () => import('../components/students/Detail.vue'),
+    meta: {
+      title: 'Detalhes do Aluno'
+    }
   },
   {
     path: '/about',
     name: 'about',
+    meta: {
+      title: 'Sobre'
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
