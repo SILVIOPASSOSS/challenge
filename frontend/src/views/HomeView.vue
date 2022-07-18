19e470cd3744239eee02719e88db64233dd260e7
Silvio Passos <silvyopassosdossantos@hotmail.com>
2022-07-18 14:08:58 -0300

    feat: adjustments
    several adjustments:
    - Visuals;
    - Fix for exclusion logic;
    - creation of the modal to confirm student deletion

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <v-container bg fill-height grid-list-md text-xl-center fluid class="fluid">
     <v-row class="d-flex justify-center" allign="center">
       <v-col cols="10">
-         <v-card class="pa-4">
+        <v-card class="pa-4">
           <v-card-title>
             <v-spacer></v-spacer>
             <v-text-field
@@ -11,8 +11,10 @@
               label="Pesquisar"
               single-line
               hide-details
+              class="pa-0"
             >
             </v-text-field>
+
             <!-- Component Register -->
             <template>
               <v-btn
@@ -26,13 +28,14 @@
             </template>
           </v-card-title>
 
+          <!-- Table -->
           <v-data-table
             :headers="headers"
             :items="students"
             :search="search"
             :footer-props="{
               'items-per-page-text': 'Alunos por página',
-              pageText: '{0}-{1} de {2}'
+              pageText: '{0}-{1} de {2}',
             }"
           >
             <template v-slot:item.actions="{ item }">
@@ -49,7 +52,6 @@
                 </template>
                 <span>Detalhes</span>
               </v-tooltip>
-
               <v-tooltip bottom>
                 <template v-slot:activator="{ on, attrs }">
                   <v-btn
@@ -64,15 +66,60 @@
                 </template>
                 <span>Editar Usuário</span>
               </v-tooltip>
-
               <v-tooltip bottom>
                 <template v-slot:activator="{ on, attrs }">
-                  <v-btn icon color="red" v-bind="attrs" v-on="on">
-                    <v-icon small @click="deleteStudent(item)"
-                      >mdi-delete</v-icon
-                    >
-                    
-                  </v-btn>
+                  <!-- Dialog for Confirm Delete Student -->
+                  <v-dialog v-model="dialog" width="500" :retain-focus="false">
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        color="red lighten-2"
+                        dark
+                        icon
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        <v-icon small>mdi-delete</v-icon>
+                      </v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title
+                        class="
+                          d-flex
+                          headline
+                          red
+                          lighten-2
+                          justify-center
+                          white--text
+                        "
+                        primary-title
+                        dark
+                        >Atenção!</v-card-title
+                      >
+                      <v-card-text class="pa-4 font-weight-bold"
+                        >Tem certeza que deseja excluir esse
+                        Registro?</v-card-text
+                      >
+                      <v-divider></v-divider>
+                      <v-card-actions>
+                        <v-btn
+                          color="red lighten-2"
+                          type="submit"
+                          text
+                          @click="closeDialog()"
+                          >Cancelar</v-btn
+                        >
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          color="secondary"
+                          type="submit"
+                          text
+                          @click.prevent="deleteStudent(item)"
+                        >
+                          Sim
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
                 </template>
                 <span>Deletar usuário</span>
               </v-tooltip>
@@ -80,100 +127,96 @@
           </v-data-table>
         </v-card>
       </v-col>
-
-
-<!--  NOTIFICATIONS -->
-    <v-snackbar v-model="snackbar.show" :timeout="timeout" :color="snackbar.color" vertical>
-      {{snackbar.message}}
-      <v-btn dark text @click="snackbar = false">Fechar</v-btn>
-    </v-snackbar>
-<!-- END  NOTIFICATIONS -->
-      <ModalConfirm />
+      <!--  NOTIFICATIONS -->
+      <div class="d-flex text-center justify-center">
+        <v-snackbar
+          v-model="snackbar.show"
+          :timeout="timeout"
+          :color="snackbar.color"
+          vertical
+        >
+          {{ snackbar.message }}
+         <v-btn dark text @click="snackbar = false">Fechar</v-btn>
+        </v-snackbar>
+      </div>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import ModalConfirm from '../components/modals/Modal.vue'
-import axios from 'axios'
-import { API_BASE_URL } from '../config.js'
-
+import axios from "axios";
+import { API_BASE_URL } from "../config.js";
 
 export default {
-  name: 'students',
-
-  components: { ModalConfirm },
+  name: "students",
 
   data() {
     return {
-      search: '',
+      search: "",
       students: [],
       headers: [
         {
-          text: '',
-          align: 'start',
-          sortable: false
+          text: "",
+          align: "start",
+          sortable: false,
         },
-        { text: 'Registro Acadêmico', value: 'academic_record' },
-        { text: 'Nome', value: 'name' },
-        { text: 'CPF', value: 'cpf' },
-        { text: 'Ações', value: 'actions', sortable: false }
+        { text: "REGISTRO ACADÊMICO", value: "academic_record" },
+        { text: "NOME", value: "name" },
+        { text: "CPF", value: "cpf" },
+        { text: "Ações", value: "actions", sortable: false },
       ],
       snackbar: {
         show: false,
         message: null,
         color: null,
-        timeout: '2000'
+        timeout: "2000",
       },
-      dialog: ''
-    }
+      dialog: false,
+    };
   },
   mounted() {
-    this.getStudents()
+    this.getStudents();
   },
   methods: {
     async getStudents() {
-      axios.get(API_BASE_URL + '/student')
-
-          .then(response => {
-          this.students = response.data
-          console.log(students.snackbar)
+      axios
+        .get(API_BASE_URL + "/student")
+        .then((response) => {
+          this.students = response.data;
+          console.log(students.snackbar);
         })
-          .catch(error => {
-          console.log("Aconteceu um erro",error)
-        })
-        
+        .catch((error) => {
+          console.log("Aconteceu um erro", error);
+        });
     },
 
-    /*  
-    // error.response.status 
-    */
-    async deleteStudent(item) {
-      if (confirm('Deseja excluir o registro do acadêmico?')) {
-        let academic = await axios.delete(API_BASE_URL + `/student/${item.id}`)
-          .then(() => {
-            this.snackbar ={
-              message:  'Deletado com sucesso',
-              color: 'success',
-              show: true,
-              timeout: 200
-            } 
-            this.getStudents()
-            
-          })
-          .catch(error => {
-            this.snackbar = {
-                        message: 'Ops! Ocorreu um erro',
-                        color: 'error',
-                        show: true,
-                        timeout: 200
-            }
-          })
-      }
-    }
-  }
-}
+    deleteStudent(item) {
+      axios
+        .delete(API_BASE_URL + `/student/${item.id}`)
+        .then(() => {
+          this.snackbar = {
+            message: "Deletado com sucesso",
+            color: "success",
+            show: true,
+            timeout: 200,
+          };
+          this.dialog = false;
+          this.getStudents();
+        })
+        .catch((error) => {
+          this.snackbar = {
+            message: "Ops! Ocorreu um erro",
+            color: "error",
+            show: true,
+            timeout: 20,
+          };
+        });
+    },
+    closeDialog() {
+      this.dialog = false;
+      this.$refs.template.reset();
+      this.getStudents();
+    },
+  },
+};
 </script>
-
-<style>
-</style>
